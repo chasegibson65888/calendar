884631b0a1b1f2d08fd0640a054738b1a6856ac9
chasegibson65888 <55086314+chasegibson65888@users.noreply.github.com>
2020-07-13 19:48:31 -0400

    Add files via upload

---
+++ e724b640eb9c7e34bc8046272a0c0aba_ddcb328ac4c23631a8623e10f350506c.js
@@ -0,0 +1,77 @@
+var p = MindFusion.Scheduling;
+
+
+var calendar = new p.Calendar(document.getElementById("calendar"));
+
+calendar.theme = "peach";
+
+
+calendar.render();
+
+calendar.itemCreating.addEventListener(handleItemCreating);
+calendar.itemModified.addEventListener(handleItemModified);
+calendar.itemDeleting.addEventListener(handleItemDeleting);
+
+function handleItemDeleting (sender, args )
+{	
+		args.cancel = true;
+		showWarningTimeCount("delete not allow");
+}
+
+function handleItemCreating (sender, args )
+{
+	if(itemOnSunday(args.item) || itemTooLateCount(args.item))
+	{
+		args.cancel = true;
+		showWarningTimeCount("don't consider sunday and don't set any event after 8 pm");
+	}
+}
+
+function handleItemModified (sender, args )
+{
+	if(itemOnSunday(args.item) || itemTooLateCount(args.item))
+	{
+		args.item.startTime = args.oldItem.startTime;
+		args.item.endTime = args.oldItem.endTime;
+		
+		showWarningTimeCount("cannot include event Sunday or take place after 8PM");
+	}
+}
+
+function itemOnSunday (item)
+{
+	var endTime = item.endTime.clone().addMilliseconds(-1);
+	
+	if(item.startTime.dayOfWeek == 0 ||
+		 endTime.dayOfWeek == 0	)
+		return true;
+		
+		var startTime = item.startTime.clone();
+		
+		while(startTime.compareTo(endTime) <= 0)
+		{	     
+			 
+			 if(startTime.dayOfWeek == 0)					  
+				 return true;		    
+			
+			 startTime.addDays(1);
+		}
+		
+		return false;
+}
+
+
+function itemTooLateCount( item )
+{
+	if(item.startTime.hour > 20 || item.endTime.hour > 20)
+		return true;
+	
+	return false;
+}
+
+function showWarningTimeCount (message_text)
+{
+	document.getElementById('alrt').innerHTML=message_text; 
+    setTimeout(function() {document.getElementById('alrt').innerHTML='';},5000);
+	
+}
